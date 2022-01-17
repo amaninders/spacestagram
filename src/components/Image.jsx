56b4910a2f8bcc09e0fcbb64832dc1f21e9b84d4
Amaninder Singh <amandeolofficial@gmail.com>
2022-01-17 13:58:25 -0500

    clean the code

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -16,27 +16,20 @@ function Image(props) {
   const [likeText, setLikeText] = useState("like")
 
   return (
-    <>
-      <style>{`
-        .red {color: red}
-        .green {color: green}
-      `}
-      </style>
-      <div className="p-3">
-        <div className="card text-center">
-          <img src={props.src} className="card-img-top" alt="..." />
-          <div className="card-body" style={{'backgroundColor':randomColor({luminosity: 'light', hue: 'yellow', alpha: 0.1})}}>
-            <h5 className="card-title">{props.title}</h5>
-            <p>{ humanDate(props.date)}</p>
-            <p className="card-text overflow-auto">{props.description}</p>
-            <div className="d-grid gap-2">
-              <a href="#" className={cls} onClick={() => { if(likeText === "like") notify('Liked 👍') ; setCls((cls) => (cls === "btn btn-outline-dark" ? "btn btn-success" : "btn btn-outline-dark")) ; setLikeText((likeText) => (likeText === "unlike" ? "like" : "unlike"))}}><i className="fas fa-heart"></i> {likeText}</a>
-              <button className="btn btn-outline-dark" onClick={ () => { notify('link copied 📋'); copy(props.src);} }><i class="far fa-copy"></i> copy url</button>
-            </div>
+    <div className="p-3">
+      <div className="card text-center">
+        <img src={props.src} className="card-img-top" alt="..." />
+        <div className="card-body" style={{'backgroundColor':randomColor({luminosity: 'light', hue: 'yellow', alpha: 0.1})}}>
+          <h5 className="card-title">{props.title}</h5>
+          <p>{ humanDate(props.date)}</p>
+          <p className="card-text overflow-auto">{props.description}</p>
+          <div className="d-grid gap-2">
+            <a href="#" className={cls} onClick={() => { if(likeText === "like") notify('Liked 👍') ; setCls((cls) => (cls === "btn btn-outline-dark" ? "btn btn-success" : "btn btn-outline-dark")) ; setLikeText((likeText) => (likeText === "unlike" ? "like" : "unlike"))}}><i className="fas fa-heart"></i> {likeText}</a>
+            <button className="btn btn-outline-dark" onClick={ () => { notify('link copied 📋'); copy(props.src);} }><i class="far fa-copy"></i> copy url</button>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
