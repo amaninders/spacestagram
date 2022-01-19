aa1644e4bceea97c8d7c3059c376820fbc80cdc4
Amaninder Singh <amandeolofficial@gmail.com>
2022-01-19 13:10:50 -0500

    changed theme

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react'
 import humanDate from '../helpers/humanDate'
 import copy from 'copy-to-clipboard';
-import randomColor from 'randomcolor';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
-
 
 function Image(props) {
 
@@ -12,19 +10,26 @@ function Image(props) {
     toast(message)
   };
 
-  const [cls, setCls] = useState("btn btn-success")
+  const [cls, setCls] = useState("btn btn-dark")
   const [likeText, setLikeText] = useState("like")
 
   return (
     <div className="p-3">
-      <div className="card text-center">
-        <img src={props.src} className="card-img-top" alt="..." />
-        <div className="card-body" style={{'backgroundColor':randomColor({luminosity: 'light', hue: 'yellow', alpha: 0.1})}}>
-          <h5 className="card-title">{props.title}</h5>
-          <p>{ humanDate(props.date)}</p>
-          <p className="card-text overflow-auto">{props.description}</p>
-          <div className="d-grid gap-2">
-            <button className={cls} onClick={() => { if(likeText === "like") notify('Liked 👍') ; setCls((cls) => (cls === "btn btn-success" ? "btn btn-danger" : "btn btn-success")) ; setLikeText((likeText) => (likeText === "unlike" ? "like" : "unlike"))}}><i className="fas fa-heart"></i> {likeText}</button>
+      <div className="card text-center" style={{'backgroundColor':'#DAE8F7'}}>
+        <img className="card-img-top p-2 pb-0" src={props.src} alt="..." />
+        <div className="card-body p-0">
+          <div className="d-flex justify-content-between align-items-stretch border border-dark mb-3 ms-2 me-2">
+            <div className='d-flex align-items-center bg-light p-3 m-0 w-75'>
+              <h5 className="card-title m-0 text-break">{props.title}</h5>
+            </div>
+            <div className="d-flex align-items-center bg-secondary p-2 m-0">
+              <h5 className="text-light text-uppercase custom-date">{ humanDate(props.date)}</h5>
+            </div>
+          </div>
+          <p className="card-text overflow-auto pt-1 ps-3 pe-3" style={{ 'textAlign': 'justify',
+  'textJustify': 'inter-word'}}>{props.description}</p>
+          <div className="d-grid gap-2 p-2">
+            <button className={cls} onClick={() => { if(likeText === "like") notify('Liked 👍') ; setCls((cls) => (cls === "btn btn-dark" ? "btn btn-danger" : "btn btn-dark")) ; setLikeText((likeText) => (likeText === "unlike" ? "like" : "unlike"))}}><i className="fas fa-heart"></i> {likeText}</button>
             <button className="btn btn-dark" onClick={ () => { notify('link copied 📋'); copy(props.src);} }><i class="far fa-copy"></i> copy url</button>
           </div>
         </div>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navigation() {
   return (
-    <nav className="navbar navbar-light bg-warning sticky-top">
+    <nav className="navbar navbar-light sticky-top"  style={{'backgroundColor':'#B2CEEC'}}>
       <div className="container">
         <a className="navbar-brand" href="/">
           <i className="fas fa-rocket"></i> Spacestagram
